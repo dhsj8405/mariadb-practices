44a415dec3ff144ada1eb5c0037d0446101b1e6c
dhsj8405 <dhsj8405@naver.com>
2021-10-01 20:23:59 +0900

    프랙티스 1,2

--- sql_practices/연습문제/practice02.sql
+++ sql_practices/연습문제/practice02.sql
@@ -3,20 +3,42 @@
 -- 보세요. 두 임금의 차이는 얼마인가요? 함께 “최고임금 – 최저임금”이란 타이틀로 출력해
 -- 보세요.
 
+select max(salary) as '최고임금',
+ min(salary) as '최저임금', 
+ max(salary) - min(salary) as '최고임금 - 최저임금'
+from salaries;
 
 -- 문제2.
 -- 마지막으로 신입사원이 들어온 날은 언제 입니까? 다음 형식으로 출력해주세요.
 -- 예) 2014년 07월 10일
+select date_format(max(hire_date), '%Y년 %c월 %d일') as 입사일
+from employees;
 
 -- 문제3.
 -- 가장 오래 근속한 직원의 입사일은 언제인가요? 다음 형식으로 출력해주세요.
 -- 예) 2014년 07월 10일
 
+select period_diff(date_format( to_date, '%Y%m'),date_format(from_date,'%Y%m')) as year
+  from titles;
+
 -- 문제4.
 -- 현재 이 회사의 평균 연봉은 얼마입니까?
+select avg(salary)
+  from salaries
+  where to_date like '9999%';
 
 -- 문제5.
 -- 현재 이 회사의 최고/최저 연봉은 얼마입니까?
+select (salary), to_date
+  from salaries
+where to_date like '9999%'
+  order by salary desc;
+
+select max(salary),min(salary)
+  from salaries
+ where to_date like '9999%';
 
 -- 문제6.
 -- 최고 어린 사원의 나이와 최 연장자의 나이는?
+select date_format(max(birth_date),'%Y') - date_format(min(birth_date),'%Y')
+from employees;
