6bafecb43bedc623ff9ae21b0afb8895b462d378
dhsj8405 <dhsj8405@naver.com>
2021-10-07 20:24:59 +0900

    bookmall sql

--- jdbc-practices/sql/평가과제.sql
+++ jdbc-practices/sql/평가과제.sql
@@ -5,12 +5,14 @@ select * from book;
 
 -- insert category
 insert into category values(null, '수필');
-select no, name from category;
+select ct.no, ct.name 
+from category ct;
 
 
 -- insert book
 insert into book values(null, 'abc' , 2000 , 1 );
-select *
+
+select a.no as book_no, a.title as 책이름, a.price as 가격 ,b.no as category_num
 from book a, category b
 where a.category_no = b.no;
 
@@ -26,16 +28,45 @@ where a.category_no = b.no;
 
 -- insert member
 insert into member values(null, '우동현', 'dhsj8405@naver.com', 1234, '010-2316-8405');
+select m.no,m.name,m.email,m.password,m.phone_number
+from member m;
 select * from member;
+
 
 -- insert cart
 insert into cart values(1,1,2);
-select * from cart;
+
+select b.no as book_no, m.no as member_no, c.quantity
+from cart c, book b, member m
+where c.book_no = b.no 
+and  c.member_no = m.no;
+
+select * from member a,cart b, book c
+where a.no = b.member_no
+and b.book_no = c.no;
+
 
 -- insert order
   -- 1기본
 insert into `order` values('희동이',5000,'test@test.com',2);
-select * from `order`;
- -- 2 
+-- select order
+select o.no as order_num , o.name, o.payment, o.address, m.no as member_no
+from `order` o, member m
+where o.member_no = m.no;
+
+
+
+-- order_book
+select * 
+from order_book;
+
+
+select * from member m, cart c, book b, `order` o ,order_book ob, category ct
+where m.no = c.member_no
+and c.book_no = b.no
+and o.member_no = m.no
+and ob.order_no = o.no;
+
+  
 
 
